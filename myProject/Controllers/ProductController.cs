2e92713038302b218b373fdebc6a42ca0ae279bd
Rajendra Bhatta <rajenbhatta56@gmail.com>
2025-05-24 15:27:05 +0545

    added new product controller

---
+++ myProject/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace myProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
